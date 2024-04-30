61d826be3a465b2475a0d2f51781d2f1595ef55d
unknown <brandenek03912@hotmail.com>
2024-04-30 13:21:18 -0400

    core: patch some save display methods

--- Blasphemous.BetterSaves/BetterSaves.cs
+++ Blasphemous.BetterSaves/BetterSaves.cs
@@ -1,12 +1,20 @@
 ﻿using Blasphemous.ModdingAPI;
 using Framework.Managers;
 using System;
+using UnityEngine;
 
 namespace Blasphemous.BetterSaves;
 
 public class BetterSaves : BlasMod
 {
     public BetterSaves() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    private int _currentMultiplier = 0;
+
+    public int GetRealSlot(int slot)
+    {
+        return slot + 3 * _currentMultiplier;
+    }
 
     protected override void OnInitialize()
     {
@@ -17,6 +25,14 @@ public class BetterSaves : BlasMod
     {
         string name = DateTime.Now.ToString();
         SetSaveName(name);
+    }
+
+    protected override void OnLateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad9))
+            _currentMultiplier++;
+        if (Input.GetKeyDown(KeyCode.Keypad8))
+            _currentMultiplier--;
     }
 
     public void SetSaveName(string name)
---
+++ Blasphemous.BetterSaves/SlotPatches.cs
@@ -0,0 +1,40 @@
+﻿using Framework.Managers;
+using Gameplay.UI.Others.MenuLogic;
+using Gameplay.UI.Widgets;
+using HarmonyLib;
+
+namespace Blasphemous.BetterSaves;
+
+[HarmonyPatch(typeof(SelectSaveSlots), nameof(SelectSaveSlots.OnSelectedSlots))]
+class Slot_Select_Patch
+{
+    public static void Prefix(ref int idxSlot)
+    {
+        //Main.BetterSaves.LogError($"Select slot {idxSlot} to {idxSlot + 3 * Main.BetterSaves.CurrentMultiplier}");
+        //idxSlot += 3 * Main.BetterSaves.CurrentMultiplier;
+    }
+}
+
+[HarmonyPatch(typeof(SelectSaveSlots), nameof(SelectSaveSlots.OnAcceptSlots))]
+class Slot_Accept_Patch
+{
+    public static void Prefix(ref int idxSlot)
+    {
+        //Main.BetterSaves.LogError($"Accept slot {idxSlot} to {idxSlot + 3 * Main.BetterSaves.CurrentMultiplier}");
+        //idxSlot += 3 * Main.BetterSaves.CurrentMultiplier;
+    }
+}
+[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.GetSlotData))]
+class PM_GetSlotData_Patch
+{
+    public static void Prefix(ref int slot) => slot = Main.BetterSaves.GetRealSlot(slot);
+}
+
+[HarmonyPatch(typeof(SaveSlot), nameof(SaveSlot.SetNumber))]
+class SaveSlot_Number_Patch
+{
+    public static void Prefix(ref int slot) => slot = Main.BetterSaves.GetRealSlot(slot);
+}
+
+//[HarmonyPatch(typeof(PersistentManager), nameof(PersistentManager.SaveGame))]
+//class PM_Save_Patch { }
