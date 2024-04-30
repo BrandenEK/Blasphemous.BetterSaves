ee953ab0fe9f89bb6255a226aa3c9b1a47b62594
unknown <brandenek03912@hotmail.com>
2024-04-30 12:41:36 -0400

    init: add base project files

---
+++ Blasphemous.BetterSaves/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.BetterSaves;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.1.2")]
+public class Main : BaseUnityPlugin
+{
+    public static BetterSaves BetterSaves { get; private set; }
+
+    private void Start()
+    {
+        BetterSaves = new BetterSaves();
+    }
+}
