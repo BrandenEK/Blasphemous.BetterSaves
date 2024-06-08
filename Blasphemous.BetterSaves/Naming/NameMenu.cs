c19740b6c256b809cc04799858df73b8e2c9846a
unknown <brandenek03912@hotmail.com>
2024-06-08 12:21:59 -0400

    naming: add text option on menu for name

--- Blasphemous.BetterSaves/Naming/NameMenu.cs
+++ Blasphemous.BetterSaves/Naming/NameMenu.cs
@@ -1,4 +1,6 @@
 ﻿using Blasphemous.Framework.Menus;
+using Blasphemous.Framework.Menus.Options;
+using Framework.Managers;
 using UnityEngine;
 
 namespace Blasphemous.BetterSaves.Naming;
@@ -13,8 +15,27 @@ public class NameMenu : ModMenu
     /// </summary>
     public NameMenu() : base("Better Saves Settings", 5) { }
 
+    private TextOption _nameOption;
+
     protected override void CreateUI(Transform ui)
     {
-        
+        TextCreator creator = new(this)
+        {
+            TextSize = 54,
+            LineSize = 500,
+        };
+
+        _nameOption = creator.CreateOption("nameoption", ui, Vector2.zero, "Save file name:", false, true, 32);
+    }
+
+    public override void OnFinish()
+    {
+        Main.BetterSaves.LogWarning($"Completing saves menu with text: ({_nameOption.CurrentValue})");
+
+        if (string.IsNullOrEmpty(_nameOption.CurrentValue))
+            return;
+
+        Main.BetterSaves.LogError("Current save slot: " + PersistentManager.GetAutomaticSlot());
+        Main.BetterSaves.NameHandler.TrySetSaveName(_nameOption.CurrentValue);
     }
 }
