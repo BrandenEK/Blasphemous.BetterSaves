4f8865fc1c62b09ac98e19704e74e3c9fc14f250
unknown <brandenek03912@hotmail.com>
2024-07-30 14:09:51 -0400

    core: update to api v2.4

--- Blasphemous.BetterSaves/Main.cs
+++ Blasphemous.BetterSaves/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.BetterSaves;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "2.2.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "2.4.0")]
 [BepInDependency("Blasphemous.CheatConsole", "1.0.0")]
 [BepInDependency("Blasphemous.Framework.Menus", "0.3.0")]
 internal class Main : BaseUnityPlugin
