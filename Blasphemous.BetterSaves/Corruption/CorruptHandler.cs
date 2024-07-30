9181845fe09ad5c7d69941b3ea13816487a2bd30
unknown <brandenek03912@hotmail.com>
2024-07-30 10:23:03 -0400

    corrupt: display box when accepting a slot

--- Blasphemous.BetterSaves/Corruption/CorruptHandler.cs
+++ Blasphemous.BetterSaves/Corruption/CorruptHandler.cs
@@ -1,30 +1,51 @@
 ﻿using Blasphemous.ModdingAPI;
 using Gameplay.UI;
+using Gameplay.UI.Others.MenuLogic;
+using UnityEngine;
 
 namespace Blasphemous.BetterSaves.Corruption;
 
 public class CorruptHandler
 {
-    public bool IsShowingConfirmation { get; private set; }
+    public bool IsShowingConfirmation => _isShowing;
+
+    private bool _isShowing = false;
+    private bool _pressedAccept = false;
+    private int _currentSlot = -1;
 
     public void TempUpdate()
     {
         if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.P))
         {
-            IsShowingConfirmation = true;
-            UIController.instance.ShowConfirmationWidget("These mods are missing [Randomizer, Multiworld]. Are you sure you want to continue?", OnAccept, OnDissent);
         }
+    }
+
+    public bool ShouldDisplayBox(int slot)
+    {
+        if (_pressedAccept)
+        {
+            _pressedAccept = false;
+            return false;
+        }
+
+        ModLog.Error("Opening slot " + slot);
+        _isShowing = true;
+        _currentSlot = slot;
+        UIController.instance.ShowConfirmationWidget("These mods are missing [Randomizer, Multiworld]. Are you sure you want to continue?", OnAccept, OnDissent);
+        return true;
     }
 
     private void OnAccept()
     {
-        ModLog.Warn("Aceept slots");
-        IsShowingConfirmation = false;
+        ModLog.Warn("Accept slots");
+        _isShowing = false;
+        _pressedAccept = true;
+        Object.FindObjectOfType<SelectSaveSlots>().OnAcceptSlots(_currentSlot);
     }
 
     private void OnDissent()
     {
         ModLog.Warn("Said no to slots");
-        IsShowingConfirmation = false;
+        _isShowing = false;
     }
 }
--- Blasphemous.BetterSaves/Corruption/CorruptPatches.cs
+++ Blasphemous.BetterSaves/Corruption/CorruptPatches.cs
@@ -11,3 +11,13 @@ class SaveSlots_Update_Patch
 {
     public static bool Prefix() => !Main.BetterSaves.CorruptHandler.IsShowingConfirmation;
 }
+
+/// <summary>
+/// Possibly open save confirmation box when loading a save file
+/// </summary>
+[HarmonyPatch(typeof(SelectSaveSlots), nameof(SelectSaveSlots.OnAcceptSlots))]
+class SaveSlots_Accept_Patch
+{
+    [HarmonyPriority(Priority.First)]
+    public static bool Prefix(ref int idxSlot) => !Main.BetterSaves.CorruptHandler.ShouldDisplayBox(idxSlot);
+}
